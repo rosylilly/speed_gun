827ba26405d5e4337c7b973f6a8e51352d318c6d
Sho Kusano <rosylilly@aduca.org>
2014-02-17 02:05:01 +0900

    Add documentation into SpeedGun::Event

--- lib/speed_gun/event.rb
+++ lib/speed_gun/event.rb
@@ -1,5 +1,18 @@
 require 'speed_gun'
 
 class SpeedGun::Event
-  attr_accessor :profile_id, :id, :name, :started_at, :finished_at, :payload
+  # @return [String] Profile ID(UUID)
+  attr_reader :profile_id
+
+  # @return [String] Event ID(UUID)
+  attr_reader :id
+
+  # @return [String] Event name
+  attr_reader :name
+
+  # @return [Time] Start time
+  attr_reader :started_at
+
+  # @return [Time, nil] Finish time
+  attr_reader :finished_at
 end
